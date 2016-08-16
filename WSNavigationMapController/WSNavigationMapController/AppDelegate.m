2c0bb6308f3464268ea978038b4527d39e7c7713
ws <ws4715535@foxmail.com>
2016-08-16 16:28:58 +0800

    增加导航

--- WSNavigationMapController/WSNavigationMapController/AppDelegate.m
+++ WSNavigationMapController/WSNavigationMapController/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +16,13 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    ViewController *firstController = [ViewController initFromStoryBoard];
+    UINavigationController *mainNavi = [[UINavigationController alloc] initWithRootViewController:firstController];
+    self.window.rootViewController = mainNavi;
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
