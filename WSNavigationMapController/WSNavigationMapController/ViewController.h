aac0a76e85a5ce00a228efd99dd9b98796834c95
ws <ws4715535@foxmail.com>
2016-08-16 15:31:59 +0800

    init

---
+++ WSNavigationMapController/WSNavigationMapController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSNavigationMapController
+//
+//  Created by ws on 16/8/16.
+//  Copyright © 2016年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WSNavigationMapController/WSNavigationMapController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WSNavigationMapController
+//
+//  Created by ws on 16/8/16.
+//  Copyright © 2016年 ws. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
